a9c54360ab4540601a8649cac84385b7162cbb6a
Vix <vild3530@ut.edu.vn>
2026-08-25 23:18:27 +0700

    Update jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
